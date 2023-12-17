630142c201919bf943a6028b693075b5df49d273
husseinmdarman <husseinmdarman@gmail.com>
2023-12-17 04:59:30 +0000

    completed file structure of raw data

---
+++ file_directory_create.py
@@ -0,0 +1,43 @@
+import json
+import os
+
+class FileDirectory():
+
+    def __init__(self, folder_name: str):
+        """
+        Takes the folder name that you would like to create in your
+        parent directory
+
+        Input: folder_name (str)
+        """
+        parent_dir = os.path.dirname(os.getcwd())
+        self.path = os.path.join(parent_dir, folder_name)
+    
+    def create_folder(self):
+        """
+        Checks whether folder is created if not, creates the folder
+        """
+        
+        if os.path.exists(self.path) == False:
+            os.mkdir(self.path)
+
+    def create_crypto_folders(self, crypto_list: dict):
+
+        for crypto_dict in crypto_list:
+            # {'Coin name': 'Bitcoin', 'Price': '$41,892.99', 'Market cap ': '1.00% $819.94B', 
+            #   'Volume (24h) ': '29.56% $13.85B', 'Volume/Market cap (24h) ': 
+            #   '1.71%', 'Circulating supply ': '19.57M BTC', 'Total supply ': '19.57M BTC', 
+            #   'Max. supply ': '21M BTC', 'Fully diluted market cap ': '$879.78B'}, 
+            crypto_folder = crypto_dict['Coin name']
+            crypto_folder_path = os.path.join(self.path, crypto_folder)
+
+            if os.path.exists(crypto_folder_path) == False:
+                os.mkdir(self.path)
+            
+            with open(os.path.join(crypto_folder_path, 'data.json'), 'w') as fp:
+                json.dump(crypto_dict, fp)
+
+if __name__ == "__main__":
+    file_create = FileDirectory('raw_data')
+    file_create.create_folder()
+    file_create.create_crypto_folders([{'Coin name': 'Bitcoin', 'Price': '$41,892.99', 'Market cap ': '1.00% $819.94B', 'Volume (24h) ': '29.56% $13.85B', 'Volume/Market cap (24h) ': '1.71%', 'Circulating supply ': '19.57M BTC', 'Total supply ': '19.57M BTC', 'Max. supply ': '21M BTC', 'Fully diluted market cap ': '$879.78B'}, {'Coin name': 'Ethereum', 'Price': '$2,213.85', 'Market cap ': '1.77% $266,104,402,167', 'Volume (24h) ': '36.61% $6,487,025,456', 'Volume/Market cap (24h) ': '2.46%', 'Circulating supply ': '120,199,702 ETH', 'Total supply ': '120,199,702 ETH', 'Max. supply ': '∞', 'Fully diluted market cap ': '$266,273,107,112'}])
